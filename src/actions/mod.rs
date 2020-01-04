0fb4975149c37ef8b2ac5c36f5114365b6f2e1fb
Jason Dixon <jason.dixon.email@gmail.com>
2020-01-05 08:36:57 +1300

    working out new struct

--- src/actions/mod.rs
+++ src/actions/mod.rs
@@ -8,9 +8,11 @@ pub mod prelude;
 mod transport;
 
 const ACTION: &'static str = "action";
+const TARGET: &'static str = "target";
 
 // Register actions with their associated logic and serialized IDs
 register_for_creep! {
     (field=ACTION, name=Action),
     HarvestEnergy(transport::HarvestEnergy) = 1,
+    DeliverEnergy(transport::DeliverEnergy) = 2,
 }
--- src/actions/transport.rs
+++ src/actions/transport.rs
@@ -1,48 +1,46 @@
 // Actions relating to transporting goods from one place to another. eg energy
 
-use crate::actions::prelude::*;
-use crate::actions::Action;
+use super::prelude::*;
+use super::{Action, TARGET};
 use crate::prelude::*;
 use log::*;
-use screeps::{game, Creep, ResourceType, ReturnCode, Source};
+use screeps::{prelude::*, game, Creep, ResourceType, ReturnCode, Source};
 use std::convert::From;
 
-const SOURCE: &'static str = "source";
-
 pub struct HarvestEnergy {
-    source: Option<Source>,
+    target: Option<Source>,
 }
 
 impl Action {
     pub fn harvest_energy(source: Source) -> Action {
         Action::HarvestEnergy(HarvestEnergy {
-            source: Some(source),
+            target: Some(source),
         })
     }
 }
 
 impl From<&Creep> for HarvestEnergy {
     fn from(creep: &Creep) -> Self {
-        if let Ok(Some(source)) = creep.memory().get(SOURCE) {
+        if let Ok(Some(source)) = creep.memory().get(TARGET) {
             return Self {
-                source: Some(source),
+                target: Some(source),
             };
         }
-        Self { source: None }
+        Self { target: None }
     }
 }
 
 impl Actionable for HarvestEnergy {
     fn save(&self, creep: &Creep) {
-        if let Some(source) = &self.source {
-            creep.memory().set(SOURCE, source);
+        if let Some(source) = &self.target {
+            creep.memory().set(TARGET, source);
         }
     }
     fn execute(&self, creep: &Creep) -> bool {
         if creep.is_full(ResourceType::Energy) {
             return false;
         }
-        if let Some(source) = &self.source {
+        if let Some(source) = &self.target {
             match creep.harvest(source) {
                 ReturnCode::Ok => {
                     if game::time() % 5 == 0 {
@@ -59,3 +57,55 @@ impl Actionable for HarvestEnergy {
         false
     }
 }
+
+// Deliver energy
+pub struct DeliverEnergy<T: HasPosition + HasStore> {
+    target: Option<T>,
+}
+
+impl Action {
+    pub fn deliver_energy<T: HasPosition + HasStore>(target: T) -> Action {
+        Action::DeliverEnergy(DeliverEnergy {
+            source: Some(target),
+        })
+    }
+}
+//
+// impl From<&Creep> for DeliverEnergy {
+//     fn from(creep: &Creep) -> Self {
+//         if let Ok(Some(target)) = creep.memory().get(TARGET) {
+//             return Self {
+//                 target: Some(target),
+//             };
+//         }
+//         Self { target: None }
+//     }
+// }
+//
+// impl Actionable for DeliverEnergy<T: HasPosition + HasStore> {
+//     fn save(&self, creep: &Creep) {
+//         if let Some(target) = &self.target {
+//             creep.memory().set(TARGET, target);
+//         }
+//     }
+//     fn execute(&self, creep: &Creep) -> bool {
+//         if creep.is_empty(ResourceType::Energy) {
+//             return false;
+//         }
+//         if let Some(source) = &self.source {
+//             match creep.transfer_all(source, ResourceType::Energy) {
+//                 ReturnCode::Ok => {
+//                     if game::time() % 5 == 0 {
+//                         creep.say("⏳", true);
+//                     }
+//                 }
+//                 ReturnCode::NotInRange => {
+//                     creep.move_to(source);
+//                 }
+//                 x => warn!("Failed to harvest: {:?}", x),
+//             }
+//             return true; // Need more energy!
+//         }
+//         false
+//     }
+// }
